6837d2e18114467989654ab4a364a910de1fad8e
Lucas A <la0359813@gmail.com>
2020-11-06 17:27:04 -0300

    Tela de login

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,33 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './style.scss';
+import logo from '../../assets/EVLAV.svg';
+
+function Login(): React.FunctionComponentElement<void> {
+  // todo form submit
+  return (
+    <div className="background">
+      <div className="content">
+        <form className="login">
+          <img src={logo} alt="logo" />
+          <label>
+            Email:
+            <input type="text" />
+          </label>
+          <label>
+            Senha:
+            <input type="password" />
+          </label>
+          <input type="submit" value="Entrar" />
+        </form>
+
+        <Link to="/">Cadastre-se</Link>
+        <Link to="/">Esqueceu a senha?</Link>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
