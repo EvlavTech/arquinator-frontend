dfff26e9548a48a406d401ba204efd8c51bdb0d3
Lucas Andrade <la0359813@gmail.com>
2021-02-04 16:23:39 -0300

    Tela de projetos

---
+++ src/pages/Projects/index.js
@@ -0,0 +1,129 @@
+import React from 'react';
+import {
+  FaFolder,
+  FaRegCheckCircle,
+  FaCheck,
+  FaEye,
+  FaTrash,
+} from 'react-icons/fa';
+
+import { Progress } from 'antd';
+import 'antd/lib/progress/style/index.css';
+import MainLayout from 'components/MainLayout';
+import MainCard from 'components/MainCard';
+import dropDown from 'assets/dropDown.svg';
+
+import testJson from './project-test';
+
+import style from './Projects.module.scss';
+
+const Projects = () => {
+  const mediaSize = window.innerWidth;
+
+  function truncate(text) {
+    if (mediaSize < 900 && text.length > 7) {
+      return `${text.substr(0, 7)}...`;
+    }
+
+    return text;
+  }
+
+  function hideComponent(component) {
+    const children = [...component.current.children];
+
+    if (children !== undefined) {
+      // eslint-disable-next-line no-console
+      console.log(children);
+      children.forEach((element) => {
+        const liElementState = element.getAttribute('hidden');
+        if (liElementState === null) {
+          element.setAttribute('hidden', '');
+        } else {
+          element.removeAttribute('hidden');
+        }
+      });
+    }
+  }
+
+  function generateComponent(projectComponent) {
+    const id = Math.random().toString();
+    const haveTasks = projectComponent.tasks !== null;
+    const liRef = React.createRef();
+
+    const component = (
+      <li className={style.liStyle} id={id} ref={liRef}>
+        <div className={style.liInformation}>
+          <div className={style.leftIcons}>
+            <img src={dropDown} alt="tree" />
+            <FaRegCheckCircle
+              className={
+                projectComponent.done ? style.completedSvg : style.none
+              }
+            />
+            <p
+              className={projectComponent.done ? style.completedP : style.none}
+            >
+              {truncate(projectComponent.name)}
+            </p>
+          </div>
+
+          <div className={style.rigthIcons}>
+            <FaCheck />
+            {haveTasks ? <FaEye onClick={() => hideComponent(liRef)} /> : null}
+            <FaTrash />
+          </div>
+        </div>
+        {haveTasks ? (
+          <ul>{projectComponent.tasks.map((e) => generateComponent(e))}</ul>
+        ) : null}
+      </li>
+    );
+
+    return component;
+  }
+
+  function generateProjectView() {
+    const projectTsx = testJson.projects.map((projectComponent) => {
+      const projectRef = React.createRef();
+      return (
+        <ul className={style.project} ref={projectRef}>
+          <div className={style.projectInformation}>
+            <div className={style.leftIcons}>
+              <FaFolder />
+              <p>{truncate(projectComponent.name)}</p>
+            </div>
+
+            <div className={style.progress}>
+              <Progress
+                strokeColor={{
+                  '0%': '#3b7b20',
+                  '100%': '#87d068',
+                }}
+                percent={projectComponent.progress}
+                trailColor="#d2d2d4"
+                showInfo={false}
+              />
+            </div>
+
+            <div className={style.rigthIcons}>
+              <FaCheck />
+              <FaEye onClick={() => hideComponent(projectRef)} />
+              <FaTrash />
+            </div>
+          </div>
+          {projectComponent.tasks.map((e) => generateComponent(e))}
+        </ul>
+      );
+    });
+
+    return projectTsx;
+  }
+
+  return (
+    <MainLayout>
+      <MainCard title="Projetos">{generateProjectView()}</MainCard>
+    </MainLayout>
+  );
+};
+
+export default Projects;
